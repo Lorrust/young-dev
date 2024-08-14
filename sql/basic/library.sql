6b04eaf3131ee892ed7b46c529c35be578f8d681
Lucas <lucasrs202@gmail.com>
2024-08-14 19:24:35 -0300

    Create sql directory

---
+++ sql/basic/library.sql
@@ -0,0 +1,70 @@
+﻿-- Database: library
+
+-- DROP
+DROP TABLE IF EXISTS book_author;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS category;
+
+DROP SEQUENCE IF EXISTS book_id_seq;
+DROP SEQUENCE IF EXISTS author_id_seq;
+DROP SEQUENCE IF EXISTS category_id_seq;
+
+-- CATEGORY
+CREATE SEQUENCE category_id_seq;
+
+CREATE TABLE category
+(
+    id INTEGER NOT NULL DEFAULT NEXTVAL('category_id_seq'),
+    nome VARCHAR(100) NOT NULL,
+    excluded BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT category_pk PRIMARY KEY (id)
+);
+
+-- BOOK
+CREATE SEQUENCE book_id_seq;
+
+CREATE TABLE book
+(
+    id INTEGER NOT NULL DEFAULT NEXTVAL('book_id_seq'),
+    nome VARCHAR(100) NOT NULL,
+    isbn VARCHAR(30),
+    id_category INTEGER NOT NULL,
+    excluded BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT book_pk PRIMARY KEY (id),
+
+    CONSTRAINT book_category_id_category_fk
+    FOREIGN KEY (id_category) REFERENCES category (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- AUTHOR
+CREATE SEQUENCE author_id_seq;
+
+CREATE TABLE author
+(
+    id INTEGER NOT NULL DEFAULT NEXTVAL('author_id_seq'),
+    nome VARCHAR(100) NOT NULL,
+    excluded BOOLEAN DEFAULT FALSE,
+
+    CONSTRAINT author_pk PRIMARY KEY (id)
+);
+
+-- BOOK_AUTHOR
+CREATE TABLE book_author
+(
+    id_author INTEGER NOT NULL,
+    id_book INTEGER NOT NULL,
+
+    CONSTRAINT book_author_pk PRIMARY KEY (id_author, id_book),
+
+    CONSTRAINT book_author_author_id_author_fk
+    FOREIGN KEY (id_author) REFERENCES author (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+    CONSTRAINT book_author_book_id_book_fk
+    FOREIGN KEY (id_book) REFERENCES book (id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
---
+++ sql/basic/school.sql
@@ -0,0 +1,98 @@
+﻿-- Database: school
+
+-- DROP
+DROP TABLE IF EXISTS teacher_lesson;
+DROP TABLE IF EXISTS student_group;
+DROP TABLE IF EXISTS lesson;
+DROP TABLE IF EXISTS classroom;
+DROP TABLE IF EXISTS teacher;
+
+DROP SEQUENCE IF EXISTS classroom_id_seq;
+DROP SEQUENCE IF EXISTS teacher_id_seq;
+DROP SEQUENCE IF EXISTS lesson_id_seq;
+DROP SEQUENCE IF EXISTS student_group_id_seq;
+
+
+-- CLASSROOM
+CREATE SEQUENCE IF NOT EXISTS classroom_id_seq;
+
+CREATE TABLE IF NOT EXISTS classroom
+(
+id INTEGER NOT NULL DEFAULT NEXTVAL('classroom_id_seq'),
+code VARCHAR(10) NOT NULL,
+active BOOLEAN DEFAULT TRUE,
+
+CONSTRAINT classroom_pk PRIMARY KEY (id)
+);
+
+SELECT * FROM classroom;
+
+
+-- TEACHER
+CREATE SEQUENCE IF NOT EXISTS teacher_id_seq;
+
+CREATE TABLE IF NOT EXISTS teacher
+(
+id INTEGER NOT NULL DEFAULT NEXTVAL('teacher_id_seq'),
+nome VARCHAR(100) NOT NULL,
+active BOOLEAN DEFAULT TRUE,
+
+CONSTRAINT teacher_pk PRIMARY KEY (id)
+);
+
+
+-- class
+CREATE SEQUENCE IF NOT EXISTS class_id_seq;
+
+CREATE TABLE IF NOT EXISTS class
+(
+id INTEGER NOT NULL DEFAULT NEXTVAL('class_id_seq'),
+code VARCHAR(10) NOT NULL,
+active BOOLEAN DEFAULT TRUE,
+id_classroom INTEGER NOT NULL,
+period CHARACTER VARYING(20) NOT NULL,
+
+CONSTRAINT class_pk PRIMARY KEY (id),
+FOREIGN KEY (id_classroom) REFERENCES classroom (id)
+ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+SELECT * FROM class;
+
+-- STUDENT_GROUP
+CREATE SEQUENCE IF NOT EXISTS student_group_id_seq;
+
+CREATE TABLE IF NOT EXISTS student_group
+(
+id INTEGER NOT NULL DEFAULT NEXTVAL('student_group_id_seq'),
+id_teacher INTEGER NOT NULL,
+id_classroom INTEGER NOT NULL,
+active BOOLEAN DEFAULT TRUE,
+
+CONSTRAINT student_group_pk PRIMARY KEY (id),
+CONSTRAINT student_group_teacher_id_teacher_fk
+FOREIGN KEY (id_teacher) REFERENCES teacher (id)
+ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT student_group_classroom_id_classroom_fk
+FOREIGN KEY (id_classroom) REFERENCES classroom (id)
+ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- TEACHER_LESSON
+CREATE TABLE IF NOT EXISTS teacher_lesson
+(
+id_teacher INTEGER NOT NULL,
+id_lesson INTEGER NOT NULL,
+
+CONSTRAINT teacher_lesson_pk PRIMARY KEY(id_teacher, id_lesson),
+
+-- Teacher Constraint
+CONSTRAINT teacher_lesson_teacher_id_teacher_fk
+FOREIGN KEY (id_teacher) REFERENCES teacher (id)
+ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+-- Lesson Constraint
+CONSTRAINT teacher_lesson_lesson_id_lesson_fk
+FOREIGN KEY (id_lesson) REFERENCES lesson (id)
+ON DELETE NO ACTION ON UPDATE NO ACTION
+);
